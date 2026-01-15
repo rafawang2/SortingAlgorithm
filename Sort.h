fc2664f1c800b801534766ccd182eafa2ca5cb2e
rafawang2 <rafawcy@gmail.com>
2026-01-15 11:49:06 +0800

    feat: InsertionSort

--- Sort.h
+++ Sort.h
@@ -1,3 +1,4 @@
+# pragma once
 # include <iostream>
 # include <vector>
 #include <cstdlib> // For rand() and srand()
@@ -40,6 +41,22 @@ vector<int> BubbleSort(vector<int> vec) {
         PrintVec(vec);
         if (!swap_flag)
             break;
+    }
+    return vec;
+}
+
+vector<int> InsertSort(vector<int> vec) {
+    int n = vec.size();
+    // 從未sort的部分插入到sorted部分的適當位置
+    for (int i=1; i<n; ++i) {   // idx 0~(i-1): sorted
+        int k = vec[i];
+        int j = i-1;    // final idx for sorted part
+        while (j>=0 && k<vec[j]) {
+            vec[j+1] = vec[j];
+            --j;
+        }
+        vec[j+1] = k;
+        PrintVec(vec);
     }
     return vec;
 }
--- main.cpp
+++ main.cpp
@@ -6,5 +6,8 @@ int main() {
     vector<int> v = {50, 10, 80, 90, 70, 60, 65, 62};
     // vector<int> v = GenerateRandomVector(10, 0, 10);
     PrintVec(v);
+    cout << "Bubble Sort:\n";
     BubbleSort(v);
+    cout << "Insertion Sort\n";
+    InsertSort(v);
 }
